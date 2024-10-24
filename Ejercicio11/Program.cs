afeab89b826530b7721585ee5cfd5e26790a80b4
SebastianGarb <sebastiangarboza63@hotmail.com>
2024-10-24 09:50:46 -0500

    ejercicio

---
+++ Ejercicio11/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] numeros = new int[1];
+Random n = new Random();
+int contP = 0, contI = 0;
+Console.Write("Ingrese la cantidad de numero que desea: ");
+int num = int.Parse(Console.ReadLine());
+
+for (int x = 0; x < num; x++)
+{
+    numeros[x] = n.Next(50, 90);
+}
+Console.WriteLine("Quieres numeros pares o impares?");
+string tipo = Console.ReadLine();
+
+if (numeros[1] % 2 == 0)
+{
+    contP++;
+}
+else if (numeros[1] % 2 != 0)
+{
+    contI++;
+}
+
+if (tipo == "impar")
+{
+    Console.WriteLine(contI);
+}
+else if (tipo == ""
