4ede25bb27400f8095292b592e70dc939fb77ef5
SebastianGarb <sebastiangarboza63@hotmail.com>
2024-10-24 10:03:20 -0500

    ejercicio

--- Ejercicio11/Program.cs
+++ Ejercicio11/Program.cs
@@ -1,30 +1,35 @@
-﻿int[] numeros = new int[1];
-Random n = new Random();
+﻿
+
 int contP = 0, contI = 0;
 Console.Write("Ingrese la cantidad de numero que desea: ");
 int num = int.Parse(Console.ReadLine());
+int[] numeros = new int[num];
+Random ran = new Random();
 
 for (int x = 0; x < num; x++)
 {
-    numeros[x] = n.Next(50, 90);
+    numeros[x] = ran.Next(50, 90);
 }
 Console.WriteLine("Quieres numeros pares o impares?");
 string tipo = Console.ReadLine();
+
+string all = string.Join(", ", numeros); 
+Console.WriteLine($"Los numeros son {all}");
 
 if (numeros[1] % 2 == 0)
 {
     contP++;
 }
-else if (numeros[1] % 2 != 0)
+if (numeros[1] % 2 == 1)
 {
     contI++;
 }
 
-if (tipo == "impar")
+if (tipo == "impares")
 {
-    Console.WriteLine(contI);
+    Console.WriteLine("La cantidad son de impares son " + contI);
 }
-else if (tipo == "par")
+else if (tipo == "pares")
 {
-    Console.WriteLine(contP);
+    Console.WriteLine("La cantidad son de pares son " + contP);
 }
